f7d2a4d343a0aa542ecbce242cc7dd36e557d5ae
simon2cyrene <login.github@cyrene.fr>
2021-03-10 14:29:14 +0100

    Update

--- update.sh
+++ update.sh
@@ -5,3 +5,4 @@ git fetch
 cp /root/config/ecole/policies.json /etc/firefox/policies/policies.json
 cp /root/config/ecole/sources.list /etc/apt/sources.list
 cp /root/config/ecole/update.sh /root/update.sh
+chmod +x /root/update.sh

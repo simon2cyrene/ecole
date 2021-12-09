c7fb66d2fdc7fd070467c90d607905b0bbc64227
simon2cyrene <32913491+simon2cyrene@users.noreply.github.com>
2021-12-09 15:49:41 +0100

    Update maintenance.sh

--- maintenance.sh
+++ maintenance.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
+
+cp -r /root/config/ecole/etc/firefox/policies /etc/firefox/
+cp -r /root/config/ecole/etc/opt/chrome/policies /etc/opt/chrome/
+cp /root/config/ecole/etc/apt/sources.list /etc/apt/sources.list
 

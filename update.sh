e73ff006e7387a2995caead4c423556932257ded
simon2cyrene <32913491+simon2cyrene@users.noreply.github.com>
2021-12-09 15:56:57 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,4 +2,8 @@
 
 cd /root/config/ecole
 git pull
-bash /root/config/ecole/maintenance.sh
+cp -r /root/config/ecole/etc/firefox/policies /etc/firefox/
+cp -r /root/config/ecole/etc/opt/chrome/policies /etc/opt/chrome/
+cp /root/config/ecole/etc/apt/sources.list /etc/apt/sources.list
+cp /root/config/ecole/update.sh /root/update.sh
+chmod +x /root/update.sh

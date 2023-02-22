afe85e44ecb33de292c8dabab56276d379d69833
Simon Jacques <admin@cyrene.fr>
2023-02-22 17:11:43 +0100

    Séparation script

--- update.sh
+++ update.sh
@@ -5,6 +5,6 @@ git pull
 cp /root/config/ecole/update.sh /root/update.sh
 chmod +x /root/update.sh
 chmod +x /root/config/ecole/main.sh
-bash +x /root/config/ecole/main.sh
+bash /root/config/ecole/main.sh
 
 

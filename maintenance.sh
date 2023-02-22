5521a15411a94faac726ca0f3fb7b3020f0c3d64
Simon Jacques <admin@cyrene.fr>
2023-02-22 16:50:26 +0100

    Suppression script maintenance

--- maintenance.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cp -r /root/config/ecole/etc/firefox/policies /etc/firefox/
-cp -r /root/config/ecole/etc/opt/chrome/policies /etc/opt/chrome/
-cp /root/config/ecole/etc/apt/sources.list /etc/apt/sources.list
-

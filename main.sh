da7b97291b1b655e0600b499d15fef1399029d7e
Simon Jacques <admin@cyrene.fr>
2023-02-22 18:48:58 +0100

    conf eleve

--- main.sh
+++ main.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-cp -r /root/config/ecole/etc/firefox/policies /etc/firefox/
-cp -r /root/config/ecole/etc/opt/chrome/policies /etc/opt/chrome/
-cp /root/config/ecole/etc/apt/sources.list /etc/apt/sources.list
-cp /root/config/ecole/etc/apt/apt.conf.d/50unattended-upgrades /etc/apt/apt.conf.d/50unattended-upgrades
-cp /root/config/ecole/etc/apt/apt.conf.d/20auto-upgrades /etc/apt/apt.conf.d/20auto-upgrades
+CONFDIR="/root/config/ecole"
+
+cp -r $CONFDIR/etc/firefox/policies /etc/firefox/
+cp -r $CONFDIR/etc/opt/chrome/policies /etc/opt/chrome/
+cp $CONFDIR/etc/apt/sources.list /etc/apt/sources.list
+cp $CONFDIR/etc/apt/apt.conf.d/50unattended-upgrades /etc/apt/apt.conf.d/50unattended-upgrades
+cp $CONFDIR/etc/apt/apt.conf.d/20auto-upgrades /etc/apt/apt.conf.d/20auto-upgrades
 flatpak update
 flatpak uninstall --unused
+
+cp $CONFDIR/eleve/.local/user-places.xbel /home/eleve/.local/user-places.xbel

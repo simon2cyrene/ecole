b6afdc75aaca3d9a1369d85c5b1711f0ee65463d
Simon Jacques <admin@cyrene.fr>
2023-02-22 20:50:44 +0100

    conf root

--- main.sh
+++ main.sh
@@ -2,6 +2,7 @@
 
 CONFDIR="/root/config/ecole"
 
+# Configuration système
 cp -r $CONFDIR/etc/firefox/policies /etc/firefox/
 cp -r $CONFDIR/etc/opt/chrome/policies /etc/opt/chrome/
 cp $CONFDIR/etc/apt/sources.list /etc/apt/sources.list
@@ -10,5 +11,9 @@ cp $CONFDIR/etc/apt/apt.conf.d/20auto-upgrades /etc/apt/apt.conf.d/20auto-upgrad
 flatpak update
 flatpak uninstall --unused
 
+# Configuration root
+cp $CONFDIR/root/.bashrc /root/.bashrc
+
+# Configuration élève
 cp $CONFDIR/eleve/.local/share/user-places.xbel /home/eleve/.local/share/user-places.xbel
 chown -R eleve:eleve /home/eleve

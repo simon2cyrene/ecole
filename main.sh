d55be342af6be63fd24e4f16f480b0025313c46b
Simon Jacques <admin@cyrene.fr>
2023-02-22 17:10:52 +0100

    Séparation script

--- main.sh
+++ main.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-cd /root/config/ecole
-git pull
-cp /root/config/ecole/update.sh /root/update.sh
-chmod +x /root/update.sh
-chmod +x /root/config/ecole/main.sh
-bash +x /root/config/ecole/main.sh
+cp -r /root/config/ecole/etc/firefox/policies /etc/firefox/
+cp -r /root/config/ecole/etc/opt/chrome/policies /etc/opt/chrome/
+#cp /root/config/ecole/etc/apt/sources.list /etc/apt/sources.list
+

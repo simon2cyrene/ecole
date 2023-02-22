78b01770d27f597b837f7de23bf170f230497f2c
Simon Jacques <admin@cyrene.fr>
2023-02-22 18:26:05 +0100

    test ok

--- main.sh
+++ main.sh
@@ -5,4 +5,4 @@ cp -r /root/config/ecole/etc/opt/chrome/policies /etc/opt/chrome/
 cp /root/config/ecole/etc/apt/sources.list /etc/apt/sources.list
 cp /root/config/ecole/etc/apt/apt.conf.d/50unattended-upgrades /etc/apt/apt.conf.d/50unattended-upgrades
 cp /root/config/ecole/etc/apt/apt.conf.d/20auto-upgrades /etc/apt/apt.conf.d/20auto-upgrades
-touch /root/test.txt
+

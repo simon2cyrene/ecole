eeb61be32243d4a3dd4163688b3004ac5cdbdc95
Simon Jacques <admin@cyrene.fr>
2023-02-22 23:01:05 +0100

    XDG_DATA_DIRS

--- main.sh
+++ main.sh
@@ -8,6 +8,7 @@ cp -r $CONFDIR/etc/opt/chrome/policies /etc/opt/chrome/
 cp $CONFDIR/etc/apt/sources.list /etc/apt/sources.list
 cp $CONFDIR/etc/apt/apt.conf.d/50unattended-upgrades /etc/apt/apt.conf.d/50unattended-upgrades
 cp $CONFDIR/etc/apt/apt.conf.d/20auto-upgrades /etc/apt/apt.conf.d/20auto-upgrades
+cp $CONFDIR/etc/environment.d/10custom.conf /etc/environment.d/10custom.conf
 flatpak update
 flatpak uninstall --unused
 

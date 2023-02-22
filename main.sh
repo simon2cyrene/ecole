18e1f971ff943a6fd232844cd21c3947281e2deb
Simon Jacques <admin@cyrene.fr>
2023-02-22 17:18:44 +0100

    APT conf

--- main.sh
+++ main.sh
@@ -2,5 +2,5 @@
 
 cp -r /root/config/ecole/etc/firefox/policies /etc/firefox/
 cp -r /root/config/ecole/etc/opt/chrome/policies /etc/opt/chrome/
-#cp /root/config/ecole/etc/apt/sources.list /etc/apt/sources.list
-
+cp /root/config/ecole/etc/apt/sources.list /etc/apt/sources.list
+cp -r /root/config/ecole/etc/apt/apt.conf.d/50unattended-upgrades /etc/apt/apt.conf.d/50unattended-upgrades

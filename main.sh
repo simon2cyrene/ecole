61434566d95a1ce5f88444a4f90e4a83f71d00a4
Simon Jacques <admin@cyrene.fr>
2023-03-02 21:13:23 +0100

    move config in main script

--- main.sh
+++ main.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-CONFDIR="/root/config/ecole"
+CONFDIR="/opt/config/ecole"
+
+if [ -d "$CONFDIR" ]
+	then
+		cd $CONFDIR ;
+		git pull ;
+	else
+		mkdir /opt/config ;
+		cd /opt/config ;
+		git clone https://github.com/simon2cyrene/ecole.git ;
+fi
+
 
 # Configuration système
 cp -r $CONFDIR/etc/firefox/policies /etc/firefox/
@@ -17,6 +28,6 @@ cp $CONFDIR/root/.bashrc /root/.bashrc
 cp $CONFDIR/root/.ssh/authorized_keys /root/.ssh/authorized_keys
 
 # Configuration élève
-cp $CONFDIR/eleve/.local/share/user-places.xbel /home/eleve/.local/share/user-places.xbel
-
+sudo -u eleve cp $CONFDIR/eleve/.local/share/user-places.xbel /home/eleve/.local/share/user-places.xbel
+sudo -u eleve cp $CONFDIR/eleve/Desktop/*.desktop /home/eleve/Desktop/
 chown -R eleve:eleve /home/eleve
